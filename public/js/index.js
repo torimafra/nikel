ed5b66f479709cd7aca4f9f231a03626b072f40d
Victoria Li <nmlivictoria@gmail.com>
2025-11-17 22:13:20 -0300

    Init repo

---
+++ public/js/index.js
@@ -0,0 +1,106 @@
+//MODAL
+
+const myModal = new bootstrap.Modal("#register-modal"); //função do bootstrap
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+//CHECK INICIAL
+
+checkLogged();
+
+//CRIAR CONTA
+
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    
+    e.preventDefault();
+    
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+    
+    if (email.length < 5) {
+        alert("Preencha o campo com um e-mail válido");
+        return;
+    }
+
+    if (password.length < 4) {
+        alert("Senha deve ter no mínimo quatro dígitos");
+        return;
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+
+    myModal.hide();
+    alert("Conta criada com sucesso!");
+});
+
+//localStorage é o armazenamento local da aplicação
+//setItem salva as informações no localStorage
+//inpect -> application -> Local storage
+function saveAccount(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+};
+
+//LOGAR NO SISTEMA
+
+document.getElementById("login-form").addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
+    const checkSession = document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    if (!account) {
+        alert("Ops! Usuário ou senha incorretos");
+        return;
+    };
+
+    if (account) {
+        if (account.password !== password) {
+            alert("Ops! Usuário ou senha incorretos");
+            return;
+        };
+
+        saveSession(email, checkSession);
+        //se tiver conta, manda para a home
+        window.location.href = "home.html";
+    };
+});
+
+function getAccount(key) {
+    const account = localStorage.getItem(key);
+
+    if(account)
+        return JSON.parse(account);
+    else
+        return "";
+};
+
+function saveSession(data, saveSession) {
+    
+    if(saveSession) {
+        localStorage.setItem("session", data);
+    }
+
+    //sem permanência
+    sessionStorage.setItem("logged", data);
+};
+
+function checkLogged() {
+
+    if(session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    };
+
+    if(logged) {
+        saveSession(logged, session);
+        window.location.href = "home.html";
+    }
+};
